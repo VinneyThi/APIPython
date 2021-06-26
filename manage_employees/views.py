7b51344d9b347de8f60fa7c73bee8985485c5c44
VinneyThi <thivinney@gmail.com>
2021-06-26 17:15:27 -0300

    primeira rota configurada

--- manage_employees/views.py
+++ manage_employees/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.http import JsonResponse
 # Create your views here.
+
+
+
+
+def employee(request):
+    context = {"testes":"foi"}
+    print('testes {context}')
+    return JsonResponse(context)

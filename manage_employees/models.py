5415b370fedb75085cabfb3258cc1bf345ead66a
VinneyThi <thivinney@gmail.com>
2021-06-26 17:29:23 -0300

    migration Employee create

--- manage_employees/models.py
+++ manage_employees/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model) :
+    name = models.CharField('name',max_length=255)
+    email =  models.EmailField(max_length=255)
+    department = models.CharField('department', max_length=100)
+
